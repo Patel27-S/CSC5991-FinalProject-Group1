3ae068cef81f52879fa5d67c4d8f2679c39afc5d
bindrap <pbindra97@gmail.com>
2023-07-22 15:36:45 -0400

    app.py for flask server, working to display json data from mongodb server

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+from bson import ObjectId
+
+app = Flask(__name__)
+
+# Connect to MongoDB
+client = MongoClient('localhost', 27017)
+db = client['restaurant']  # Replace 'restaurant' with your database name
+
+# Helper function to convert ObjectId to string
+def jsonify_with_objectid(data):
+    for document in data:
+        document['_id'] = str(document['_id'])
+    return jsonify(data)
+
+# API Endpoint to fetch all restaurants
+@app.route('/api/restaurants', methods=['GET'])
+def get_restaurants():
+    restaurants = list(db['restaurants'].find({}))
+    return jsonify_with_objectid(restaurants)
+
+# API Endpoint to fetch all users
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    users = list(db['users'].find({}))
+    return jsonify_with_objectid(users)
+
+if __name__ == '__main__':
+    app.run(debug=True)
