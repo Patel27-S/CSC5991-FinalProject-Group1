428f47e5fe9568997949460a2ff0dc9fd66caabf
bindrap <pbindra97@gmail.com>
2023-07-22 10:49:00 -0400

    db intialized and script to insert/test data added, readme updated

--- DataGenerator/insert_data.py
+++ DataGenerator/insert_data.py
@@ -1,30 +1,44 @@
 import json
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 
 def read_json_file(file_name):
     with open(file_name, 'r') as file:
         data = json.load(file)
     return data
 
-# Read data from JSON files
-restaurant_data = read_json_file('restaurant_list.json')
-user_data = read_json_file('user_list.json')
+# MongoDB connection parameters
+mongo_host = 'localhost'
+mongo_port = 27017
 
-# Connect to the MongoDB server
-client = MongoClient('localhost', 27017)
+# Connect to MongoDB
+try:
+    client = MongoClient(mongo_host, mongo_port)
+    db = client['restaurant']
 
-# Create or access the 'restaurant' database
-db = client['restaurant']
+    # Read data from JSON files
+    restaurant_data = read_json_file('restaurant_list.json')
+    user_data = read_json_file('user_list.json')
 
-# Create collections for 'restaurants' and 'users'
-restaurants_collection = db['restaurants']
-users_collection = db['users']
+    # Create collections for 'restaurants' and 'users'
+    restaurants_collection = db['restaurants']
+    users_collection = db['users']
 
-# Insert data into the 'restaurants' collection
-restaurants_collection.insert_many(restaurant_data)
+    # Insert data into the 'restaurants' collection
+    restaurants_collection.insert_many(restaurant_data)
 
-# Insert data into the 'users' collection
-users_collection.insert_many(user_data)
+    # Insert data into the 'users' collection
+    users_collection.insert_many(user_data)
 
-# Close the MongoDB connection
-client.close()
+    # Query and print documents from 'restaurants' collection
+    for restaurant in restaurants_collection.find():
+        print(restaurant)
+
+    # Query and print documents from 'users' collection
+    for user in users_collection.find():
+        print(user)
+
+    # Close the MongoDB connection
+    client.close()
+    print("we got to the end")
+except errors.ConnectionFailure as e:
+    print("Connection to MongoDB failed:", e)
