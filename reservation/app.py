a13a5031004b29c73d522615017fc6461923bf66
RomanWSU <gv8414@wayne.edu>
2023-07-26 18:37:02 -0400

    Return a JSON response insted of a String

--- reservation/app.py
+++ reservation/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, app #, jsonify
+from flask import Flask, app, jsonify
 from pymongo import MongoClient
 
 #from bson import ObjectId
@@ -19,15 +19,15 @@ def jsonify_with_objectid(data):
 def book_table(restaurant_name, num_of_seats):
     restaurant = db['restaurants'].find_one({"Name": restaurant_name})
     if restaurant is None:
-        return "Restaurant not found."
+        return jsonify({"message": "Restaurant not found."}), 404
 
     available_seats = restaurant["Available_Seats"]
     if available_seats >= num_of_seats:
         new_available_seats = available_seats - num_of_seats
         db['restaurants'].update_one({"_id": restaurant["_id"]}, {"$set": {"Available_Seats": new_available_seats}})
-        return f"Table booked at {restaurant_name} for {num_of_seats} seats."
+        return jsonify({"message": f"Table booked at {restaurant_name} for {num_of_seats} seats."}), 200
     else:
-        return f"Sorry, {restaurant_name} doesn't have enough available seats for {num_of_seats} people."
+        return jsonify({"message": f"Sorry, {restaurant_name} doesn't have enough available seats for {num_of_seats} people."}), 400
     
 # API Endpoint for booking a table at a restaurant
 @app.route('/api/book/<string:Name>/<int:Seats_Required>', methods=['POST'])
