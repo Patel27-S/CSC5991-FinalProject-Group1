7efaffc7fe89701824e1214a69603d7ea15eb140
Eric Brown <ericbrown@Erics-MacBook-Air.local>
2023-07-28 12:58:56 -0400

    (Refactored addRatings method)

--- ratings/app.py
+++ ratings/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from pymongo import MongoClient
+from pymongo import MongoClient, ReturnDocument
 from bson import ObjectId
 from bson.json_util import dumps, loads
 
@@ -13,10 +13,10 @@ app = Flask(__name__)
 
 
 @app.route("/addRatings", methods=["POST"])
-def add_Ratings(rating):
-    result = int(db['restaurant'].insert_one({}).inserted_id)
-    reviewRating = int(db["restaurant"].find_one(result)) ## Storing the rating we just changed
-    return jsonify_with_objectid(reviewRating) ## displaying the rating
+def add_Ratings(restuarant,rating):
+   return (db['restaurant'].db.find_one_and_update({'name':restuarant},
+                        { '$set': { "Rating" : rating} },
+                        return_document = ReturnDocument.AFTER))
 
 
 @app.route("/FiveStarList", methods=["GET"])
