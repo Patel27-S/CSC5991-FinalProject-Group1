7295ce8fdae3c1affb2de6f62b4f63513eaeb0e7
Eric Brown <ericbrown@Erics-MacBook-Air.local>
2023-07-29 11:40:16 -0400

    Added method for getting the rating and refator addRatings method to use url params and calculate average

--- ratings/app.py
+++ ratings/app.py
@@ -1,9 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from pymongo import MongoClient, ReturnDocument
 from bson import ObjectId
 from bson.json_util import dumps, loads
 
-from restaurants.app import jsonify_with_objectid
 
 # Connect to MongoDB
 client = MongoClient("localhost", 27017)
@@ -12,18 +11,41 @@ db = client["restaurant"]  # Replace 'restaurant' with your database name
 app = Flask(__name__)
 
 
+## Calculate average rating of restaurant
+def calculate_Rating(rating, current_Rating):
+    return (rating + current_Rating) / 2
+
+
 @app.route("/addRatings", methods=["POST"])
-def add_Ratings(restuarant,rating):
-   return (db['restaurant'].db.find_one_and_update({'name':restuarant},
-                        { '$set': { "Rating" : rating} },
-                        return_document = ReturnDocument.AFTER))
+def add_Ratings():
+    input_Restaurant = request.args.get("restaurant")
+    input_Rating = request.args.get("rating")
+    current_Rating = int(
+        db["restaurant"].db.find({"name": input_Restaurant}, {"Ratings: "})
+    )  ##Gets the current ratings of that restaurant
+    return jsonify(
+        db["restaurant"].db.find_one_and_update(
+            {"name": input_Restaurant},
+            {"$set": {"Rating": calculate_Rating(input_Rating, current_Rating)}},
+            return_document=ReturnDocument.AFTER,
+        )
+    )
 
 
 @app.route("/FiveStarList", methods=["GET"])
 def five_star_list():
     # pull all 5 star restaurant from table and display
-    top_five_restaurants = dict(db['restaurant'].find({"Rating": "5"}))
-    return jsonify_with_objectid(top_five_restaurants)
+    top_five_restaurants = dict(db["restaurant"].find({"Rating": "5"}))
+    return jsonify(top_five_restaurants)
+
+
+app.route("/GetRatings", methods=["GET"])
+def get_Ratings():
+    input_Restaurant = request.args.get("restaurant")
+    current_Rating = int(
+        db["restaurant"].db.find({"name": input_Restaurant}, {"Ratings: "})
+    )
+    return jsonify(current_Rating)
 
 
 if __name__ == "__main__":
