82679aeb9266761474b53b65cdeb0e531d3c4547
Smit Patel <81384943+Patel27-S@users.noreply.github.com>
2023-07-31 19:53:01 -0400

    Refactored few services by removing inter-modular imports.

--- RestaurantByName/app.py
+++ RestaurantByName/app.py
@@ -3,13 +3,16 @@ from pymongo import MongoClient, ReturnDocument
 from bson import ObjectId
 from bson.json_util import dumps, loads
 
-from Restaurants.app import jsonify_with_objectid
 
 # Connect to MongoDB
 client = MongoClient("localhost", 27017)
 db = client["restaurant"]  # Replace 'restaurant' with your database name
 
 app = Flask(__name__)
+
+def jsonify_with_objectid(data):
+    data['_id'] = str(data['_id'])
+    return jsonify(data)
 
 
 # API Endpoint for listing all the restaurants on the basis of 
