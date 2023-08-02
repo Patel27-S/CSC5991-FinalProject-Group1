acf35b651a46ee3a2a597f79f411014e088d6f58
Eric Brown <ericbrown@Erics-MacBook-Air.local>
2023-08-02 16:52:18 -0400

    Casting fix

--- ratings/app.py
+++ ratings/app.py
@@ -20,9 +20,8 @@ def calculate_Rating(rating, current_Rating):
 def add_Ratings():
     input_Restaurant = request.args.get("restaurant")
     input_Rating = request.args.get("rating")
-    current_Rating = int(
-        db["restaurant"].db.find({"name": input_Restaurant}, {"Ratings: "})
-    )  ##Gets the current ratings of that restaurant
+    current_Rating =  db["restaurant"].db.find({"name": input_Restaurant}, {"Ratings: "})
+     ##Gets the current ratings of that restaurant
     return jsonify(
         db["restaurant"].db.find_one_and_update(
             {"name": input_Restaurant},
